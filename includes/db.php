dfc25ffac6899ecb73536e4ebbec629ee762fdbe
My laptop <samiraomar.xx@gmail.com>
2026-06-07 22:41:36 +0300

    updated

--- includes/db.php
+++ includes/db.php
@@ -1,17 +1,20 @@
 <?php
 
+require_once __DIR__ . '/../load_env.php';
+
 $conn = new mysqli(
-    $_ENV['DB_HOST'],
-    $_ENV['DB_USER'],
-    $_ENV['DB_PASS'],
-    $_ENV['DB_NAME']
+    getenv('DB_HOST'),
+    getenv('DB_USER'),
+    getenv('DB_PASS'),
+    getenv('DB_NAME')
 );
 
 if ($conn->connect_error) {
     http_response_code(500);
     echo json_encode([
         "success" => false,
-        "message" => "Database connection failed"
+        "message" => "Database connection failed",
+        "error" => $conn->connect_error
     ]);
     exit;
 }
